c115480bead9f8db4dcec5f31444fbb5411b24d7
mgomez <mgomez@mgomez-pc.cl.oracle.com>
2012-12-21 22:55:28 -0300

    hey

---
+++ merken/src/merken/util/parser/StringMatcher.java
@@ -0,0 +1,11 @@
+package merken.util.parser;
+
+public class StringMatcher extends Matcher<String> {
+
+	@Override
+	public String eval() throws ParserException {
+		// TODO Auto-generated method stub
+		return super.eval();
+	}
+
+}
